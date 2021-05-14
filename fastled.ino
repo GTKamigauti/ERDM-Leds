976e5ddd090d3627cbae3bc177771bdd479133c2
Gabriel T. Kamigauti <55719600+GTKamigauti@users.noreply.github.com>
2021-05-14 10:59:23 -0300

    Add files via upload

--- fastled.ino
+++ fastled.ino
@@ -1,10 +1,18 @@
 #include <FastLED.h>
 
-#define NUM_LEDS 60
+#define NUM_LEDS 54
 
-#define DATA_PIN 8
+#define DATA_PIN 8 //pino led
 
 #define BUTTON_PIN 6 //botão pino
+
+#define POT_PINLUZ A5 //Potenciometro de luminosidade
+
+#define Pot_PINVELO A3 //Potenciopmetro de velocidade
+
+int PotValueLUZ;
+
+int PotValueVELO;
 
 int lastclick = HIGH; //botão estado inicial
 
@@ -34,16 +42,29 @@ void setup(){
 
     pinMode(BUTTON_PIN, INPUT_PULLUP); //assignar o pino do bot]ao para pullup
 
-    FastLED.setBrightness(1);
+    FastLED.setBrightness(0);
 
     pinMode(LED_BUILTIN, OUTPUT);
+
+    pinMode(POT_PINLUZ, INPUT);
+
+    pinMode(Pot_PINVELO, INPUT);
 }
 
 void loop(){
 
+
+    PotValueLUZ = analogRead(POT_PINLUZ); //está lendo o potenciometro
+    PotValueLUZ = map(PotValueLUZ, 0, 1023, 0, 255); //mapeando os 1023 para 255
+
+    FastLED.setBrightness(PotValueLUZ);
+
+    PotValueVELO = analogRead(Pot_PINVELO);
+    PotValueVELO = map(PotValueVELO, 0, 1023, 0, 2000); //2000 velo de agr
+
     click = digitalRead(BUTTON_PIN);
     
-    if (click == LOW && lastclick == HIGH){
+    if (click == LOW && lastclick == HIGH){ //rotina de botão
         if (state == true){
             state = false;
         }
@@ -53,15 +74,15 @@ void loop(){
     }
     lastclick = click;
     
-    if(state == true){ //botão solto faz firu firu
-        dot = 0;
-        for(int count = 1; count < (NUM_LEDS*2)-1; count++){ 
-            passou = false;
+    if(state == true){ //rotina de led
+        dot = 0; //inicializa o led no 0
+        for(int count = 1; count < (NUM_LEDS*2)-1; count++){ //rotina do led que faz ir e voltar
+            passou = false; //booleano que define se o led passou
             while (state == true && passou == false){
                 n++;
 
-                if (n > velocidade){
-                    if(count > NUM_LEDS-1){
+                if (n > PotValueVELO){ // condicional que define a velocidade dos leds no lugar de um delay
+                    if(count > NUM_LEDS-1){ //condicional que faz o led ir e voltar
                         dot--;
                     }
                     else{
@@ -75,7 +96,7 @@ void loop(){
                     n = 0;
                     passou = true;
                 }
-                click = digitalRead(BUTTON_PIN);
+                click = digitalRead(BUTTON_PIN); // leitura do botão para sair da rotina de led
 
                 if (click == LOW && lastclick == HIGH){
                     if (state == true){
@@ -87,11 +108,11 @@ void loop(){
                 }
                 lastclick = click;
             }
-            if (state == false){
+            if (state == false){ //depois de apertar o botão para parar, se aplica um efeito de suavisação
                 n = 0;
-                while(dot < 30){
+                while(dot < (NUM_LEDS/2)){ //faz o led avançar para o centro lentamente 
                     n++;
-                    if (n > (velocidade)+(decre/(30-dot))){
+                    if (n > (PotValueVELO)+(decre/((NUM_LEDS/2)-dot))){
                         dot ++;
                     leds[dot] = CRGB::MediumSpringGreen; // BlueViolet
                     FastLED.show();
@@ -99,9 +120,9 @@ void loop(){
                     leds[dot] = CRGB::Black;
                     }
                 }
-                while(dot > 30){
+                while(dot > (NUM_LEDS/2)){ //faz o led voltar para o centro lentamente
                     n++;
-                    if (n > (velocidade)+(decre/(dot-30))){
+                    if (n > (PotValueVELO)+(decre/(dot-(NUM_LEDS/2)))){
                         dot --;
                     leds[dot] = CRGB::MediumSpringGreen; // BlueViolet
                     FastLED.show();
@@ -113,7 +134,7 @@ void loop(){
             }
         }
     }
-    else{
+    else{ //apaga os leds
         fill_solid(leds, NUM_LEDS, CRGB::Black);
         FastLED.show();
     }
